7c0d529195dd1e7d976179cc69c61e713559497c
spinningplates <skhnat@gmail.com>
2014-05-14 16:57:47 -0400

    adding calibration and correction MATLAB functions

---
+++ MATLAB-Code/pitch_moment_calibration.m
@@ -0,0 +1,56 @@
+function [theta]=pitch_moment_calibration(u_cal,y_cal)
+
+%=========================================================================
+%FUNCTION mx_calibration
+%      Estimates the coefficients of a second-order model using the data 
+%      from the calibration trial.  Coefficients are estimated using the   
+%      simulation and optimization function, where theta is optimized by
+%      minimizing the error between the measured and simulated pitch
+%      moment.
+%
+%--------
+%Inputs
+%--------
+%   u_cal     (Nsamples x 1)    Column vector of recorded accelerations
+%                               from the calibration trial
+%   y_cal     (Nsamples x 1)    Column vector of recorded pitch moment
+%                               from the calibration trial
+%--------
+%Outputs
+%--------
+%   theta     (6 x 1)           Column vector of the estimated coefficients
+%                               from the second-order model and
+%                               optimization
+%
+%--------------------------------------------------------------------------
+
+thetaguess=rand(6,1);
+theta=fmincon(@optimize,thetaguess,[],[],[],[],-10*ones(6,1),10*ones(6,1));
+   
+%-------------------------------------------------------------------------
+%Optimization to Reduce Error Between Simulated and Recorded Pitch Moment
+%-------------------------------------------------------------------------
+    function [f]=optimize(theta)
+    ysim=simulate(u_cal,theta);
+    f=norm(ysim-y_cal);
+    end
+%-------------------------------------------------------------------------
+%Simulation Using a Second-Order Model and the Estimated Coefficients
+%-------------------------------------------------------------------------
+    function [ysim]=simulate(uk,theta)
+    ysim=zeros(length(uk),1);
+    for i=1:length(uk)
+        if i==1
+            ysim(1,:)=theta(3,:)*uk(i)+theta(6,:);
+        else if i==2
+            ysim(2,:)=-theta(1,:)*ysim(i-1)+theta(3,:)*uk(i)...
+                      +theta(4,:)*uk(i-1)+theta(6,:);
+        else 
+            ysim(i)=-theta(1,:)*ysim(i-1)-theta(2,:)*ysim(i-2)+....
+                     theta(3,:)*uk(i)+theta(4,:)*uk(i-1)+...
+                     theta(5,:)*uk(i-2)+theta(6,:);
+            end
+        end
+    end
+    end
+    end
---
+++ MATLAB-Code/pitch_moment_correction.m
@@ -0,0 +1,45 @@
+function [mx_sim,mx_corrected]=pitch_moment_correction(u_cor,y_cor,theta)
+
+%=========================================================================
+%FUNCTION mx_correction
+%   1) Compensates for inertial errors in pitch moment due to the 
+%      rotation of the treadmill rollers.  
+%   2) Simulates the pitch moment by using recorded accelerations and 
+%      the coefficients obtained from the calibration function mx_calibration
+%   3) Compensates the pitch moment by subtracting the simulated pitch
+%      moment from the pitch moment obtained from the experiment.  
+%
+%--------
+%Inputs
+%--------
+%   u_cor     (Nsamples x 1)    Column vector of recorded accelerations
+%                               from the trial to be corrected
+%   y_cor     (Nsamples x 1)    Column vector of recorded pitch moment
+%                               from the trial to be corrected
+%   theta     (6 x 1)           Column vector coefficients obtained from the
+%                               mx_calibration function
+%--------
+%Outputs
+%--------
+%   mx_corrected (Nsamples x 1) Column vector of corrected pitch moment 
+%                               after applying the coefficients from the 
+%                               calibration trial
+%=========================================================================
+
+    ysim=zeros(length(u_cor),1);
+    for i=1:length(u_cor)
+        if i==1
+            ysim(1,:)=theta(3,:)*u_cor(i)+theta(6,:);
+        else if i==2
+            ysim(2,:)=-theta(1,:)*ysim(i-1)+theta(3,:)*u_cor(i)+...
+                       theta(4,:)*u_cor(i-1)+theta(6,:);
+        else 
+            ysim(i)=-theta(1,:)*ysim(i-1)-theta(2,:)*ysim(i-2)+...
+                     theta(3,:)*u_cor(i)+theta(4,:)*u_cor(i-1)+...
+                     theta(5,:)*u_cor(i-2)+theta(6,:);
+            end
+        end
+        mx_sim=ysim;
+        mx_corrected=y_cor-ysim;
+    end
+ end
