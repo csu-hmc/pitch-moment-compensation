baa76da749f36ec09713d302b924b75187fec3ee
spinningplates <skhnat@gmail.com>
2014-05-16 16:11:45 -0400

    eliminating the embedded simulate function

--- MATLAB-Code/pitch_moment_calibration.m
+++ MATLAB-Code/pitch_moment_calibration.m
@@ -24,33 +24,16 @@ function [theta]=pitch_moment_calibration(u_cal,y_cal)
 %
 %--------------------------------------------------------------------------
 
-thetaguess=rand(6,1);
-theta=fmincon(@optimize,thetaguess,[],[],[],[],-10*ones(6,1),10*ones(6,1));
+    thetaguess=rand(6,1);
+    theta=fmincon(@optfunc,thetaguess,[],[],[],[],-10*ones(6,1),10*ones(6,1));
    
 %-------------------------------------------------------------------------
 %Optimization to Reduce Error Between Simulated and Recorded Pitch Moment
 %-------------------------------------------------------------------------
-    function [f]=optimize(theta)
-    ysim=simulate(u_cal,theta);
-    f=norm(ysim-y_cal);
+    function [f]=optfunc(theta)
+        [ysim,~]=simulate(u_cal,theta);
+        f=norm(ysim-y_cal);
     end
-%-------------------------------------------------------------------------
-%Simulation Using a Second-Order Model and the Estimated Coefficients
-%-------------------------------------------------------------------------
-    function [ysim]=simulate(uk,theta)
-    ysim=zeros(length(uk),1);
-    for i=1:length(uk)
-        if i==1
-            ysim(1,:)=theta(3,:)*uk(i)+theta(6,:);
-        else if i==2
-            ysim(2,:)=-theta(1,:)*ysim(i-1)+theta(3,:)*uk(i)...
-                      +theta(4,:)*uk(i-1)+theta(6,:);
-        else 
-            ysim(i)=-theta(1,:)*ysim(i-1)-theta(2,:)*ysim(i-2)+....
-                     theta(3,:)*uk(i)+theta(4,:)*uk(i-1)+...
-                     theta(5,:)*uk(i-2)+theta(6,:);
-            end
-        end
-    end
-    end
-    end
+
+end
+  
